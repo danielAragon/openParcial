cf943cef5b8666db96cac5f05f07bc93016c8fe5
Daniel Aragon <daragono@everis.com>
2018-05-22 09:03:49 -0500

    crud ruta 60%

--- SpringBoot/src/main/java/com/hamp/app/controllers/RutaController.java
+++ SpringBoot/src/main/java/com/hamp/app/controllers/RutaController.java
@@ -3,6 +3,7 @@ package com.hamp.app.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,12 +21,20 @@ public class RutaController {
 		model.addAttribute("rutas", rutaService.findAll());
 		return "rutas";
 	}
-	
-	@RequestMapping(value = "/rutaAddEdit")
-	public String crear(Model model) {
+	@RequestMapping(value={"/rutaAddEdit","/rutaAddEdit/{id}"}, method = RequestMethod.GET)
+    public String rutaFindOne(Model model, @PathVariable(required = false, name = "id") Long id) {
+        if (null != id) {
+            model.addAttribute("ruta", rutaService.findOne(id));
+        } else {
+            model.addAttribute("ruta", new Ruta());
+        }
+        return "rutaAddEdit";
+    }
+	@RequestMapping(value = "/rutaAddEdit", method = RequestMethod.POST)
+	public String rutaUpdate(Model model) {
 		Ruta ruta = new Ruta();
 		model.addAttribute("ruta", ruta);
 		model.addAttribute("Titulo","Formulario Ruta");
-		return "rutaAddEdit";
+		return "rutas";
 	}
 }
--- SpringBoot/src/main/java/com/hamp/app/models/entity/Ruta.java
+++ SpringBoot/src/main/java/com/hamp/app/models/entity/Ruta.java
@@ -3,6 +3,8 @@ package com.hamp.app.models.entity;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -17,6 +19,7 @@ public class Ruta implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long ID;
 	
 	@NotEmpty
